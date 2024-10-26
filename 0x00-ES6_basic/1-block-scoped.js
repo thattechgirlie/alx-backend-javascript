2dc65fe7f57d9d2bb6831ebb6b4549f16df2f954
thattechgirlie <wanji.njoro1@gmail.com>
2024-10-26 23:36:50 +0300

    ES6

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,8 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
+  const task = false;
+  const task2 = true;
   
-    if (trueOrFalse) {
-    }
-  
-    return [task, task2];
+  if (trueOrFalse) {
+  }
+  return [task, task2];
   }
--- 0x00-ES6_basic/nodesource_setup.sh
+++ 0x00-ES6_basic/nodesource_setup.sh
@@ -1,25 +1,3 @@
-#!/bin/bash
-
-# Discussion, issues and change requests at:
-#   https://github.com/nodesource/distributions
-#
-# Script to install the NodeSource Node.js 12.x repo onto a
-# Debian or Ubuntu system.
-#
-# Run as root or insert `sudo -E` before `bash`:
-#
-# curl -sL https://deb.nodesource.com/setup_12.x | bash -
-#   or
-# wget -qO- https://deb.nodesource.com/setup_12.x | bash -
-#
-# CONTRIBUTIONS TO THIS SCRIPT
-#
-# This script is built from a template in
-# https://github.com/nodesource/distributions/tree/master/deb/src
-# please don't submit pull requests against the built scripts.
-#
-
-
 export DEBIAN_FRONTEND=noninteractive
 SCRSUFFIX="_12.x"
 NODENAME="Node.js 12.x"
@@ -417,5 +395,4 @@ print_status """Run \`${bold}sudo apt-get install -y ${NODEPKG}${normal}\` to in
 
 }
 
-## Defer setup until we have the complete script
 setup
